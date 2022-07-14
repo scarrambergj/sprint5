5250385076afc62a8efc302d51b41eb78874a0b1
Julian Scarramberg <scarrambergj@gmail.com>
2022-07-14 12:44:57 -0300

    commit

--- app.py
+++ app.py
@@ -21,40 +21,52 @@ with open('pruebaJSON.json', 'r') as file:
 
 dir_especificaciones = path.join('packages', 'JSON', 'especificaciones.json')
 
-
 with open(dir_especificaciones, 'r') as file:
     tipos_de_cuenta = json.load(file) 
 
 specs_tipo = find_in_dict(transacciones, tipos_de_cuenta, 'tipo')
 
 class Cliente():
-    def __init__(self, nombre, apellido, numero, dni, cuentas = []):
+    def __init__(self, nombre, apellido, numero, dni, transacciones = []):
         self.nombre = nombre
         self.apellido = apellido
         self.numero = numero
         self.dni = dni
 
-    def agregar_cuenta(self, cuenta):
-        self.cuentas.append(cuenta)
+    def agregar_transaccion(self, transaccion):
+        self.transacciones.append(transacciones)
 
     def agregar_direccion(direc):
         __direccion = direc
 
-    def puede_crear_chequera(self):
-        pass
-    # Debe retornar un booleano
+    def puede_crear_chequera(self, cheq_actual, max_cheq):
+        return cheq_actual < max_cheq
 
-    def puede_crear_tarjeta_credito(self):
-        pass
-    # Debe retornar un booleano
+    def puede_crear_tarjeta_credito(self, tc_actual, max_tc):
+        return tc_actual < max_tc
 
-    def puede_comprar_dolar(self):
-        pass
-    # Debe retornar un booleano
+    def puede_comprar_dolar(self, dolar):
+        return dolar
 
-class Cuenta():
-    def __init__(self):
+class Transaccion():
+    def __init__(self, estado, razon, cuenta_numero, cupo_diario_restante, cantidad_extracciones, monto, fecha, numero, saldo_cuenta, total_tc, total_c):
+        self.tipo = estado
+        self.razon = razon
+        self.cuenta_numero = cuenta_numero
+        self.cupo_diario_restante = cupo_diario_restante
+        self.cantidad_extracciones = cantidad_extracciones
+        self.monto = monto
+        self.fecha = fecha
+        self.numero = numero
+        self.saldo_cuenta = saldo_cuenta
+        self.total_tc = total_tc
+        self.total_tc = total_c
+    
+class Razon:
+    def __init__(self, tipo):
         pass
+
+    
 
 class Tipo_Cuenta(Cliente):
     def __init__(self,nombre, apellido, numero, dni, tipo, cuentas=[]):
